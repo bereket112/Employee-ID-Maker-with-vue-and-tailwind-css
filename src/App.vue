2fe0314b533af86d53ec91a1b0708478e7fa7300
bereket112 <bereketbeki64@gmail.com>
2024-08-31 16:02:27 +0300

    first page changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="container mx-auto p-4">
+  <div class="container mx-auto p-4   justify-center ">
+    <h2 class="text-3xl text-center text-sky-600 font-extralight m-4 border-b-2  border-b-sky-500">
+      Wellcome to Employee ID Maker
+    </h2>
+    <marquee class="border-[1px] rounded-md ring-[1px] ring-amber-300 border-red-500 border-b-amber-600  h-9 m-6">Please Select what U want ?</marquee>
     <div class="flex justify-center space-x-4 mb-4">
       <button @click="showForm = true" class="bg-blue-500 text-white p-2 rounded hover:bg-blue-700">Create ID Card</button>
       <button @click="showForm = false" class="bg-green-500 text-white p-2 rounded hover:bg-green-700">View ID Cards</button>
